fd95e8aacd00bc964907771a997e8619cfc02336
Vivek Jha <progamervivek2020@gmail.com>
2025-06-30 00:17:35 +0530

    .

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 var tl = gsap.timeline();
 
+tl.from("#navbar", {
+  duration: 1,
+  y: -100,
+  opacity: 0,
+  ease: "power2.out"
+});
 
- tl.from("#navbar", {
-    duration: 1,
-    y: -100,
-    opacity: 0,
-    ease: "power2.out"
+tl.from("#move .move", {
+  duration: 1,
+  x: 100,
+  opacity: 0,
+  ease: "power2.out",
+  stagger: 0.2
+});
 
-  });
+gsap.from("#me", {
+  duration: 1,
+  x: -100,
+  opacity: 0,
+  ease: "power2.out"
+});
+
+tl.from("#work", {
+  duration: 0.8,
+  opacity: 0,
+  scale: 0.5,
+  ease: "power2.out",
+  stagger: 0.2
+});
